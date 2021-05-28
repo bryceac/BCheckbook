77733270e6e1f9127d4f96b30b1dc69255e86396
Bryce Campbell <tonyhawk2100@gmail.com>
2021-05-27 21:57:49 -0700

    move content view to iOS.

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Shared
+//  Checkbook
 //
 //  Created by Bryce Campbell on 5/27/21.
 //
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
