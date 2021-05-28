79a0fc6391b5942ac2a9bc5246df8f079c3b2e5d
Bryce Campbell <tonyhawk2100@gmail.com>
2021-05-27 21:21:09 -0700

    start work on main type.

--- Shared/Event.swift
+++ Shared/Event.swift
@@ -6,3 +6,84 @@
 //
 
 import Foundation
+
+struct Event {
+    var date: Date = Date()
+    var checkNumber: Int? = nil
+    var vendor: String = ""
+    var memo: String = ""
+    var amount: Double = 0
+    var type: EventType = .withdrawal
+    var isReconciled: Bool = false
+    
+    static let DF: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    static let CURRENCY_FORMAT: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+}
+
+extension Event: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case date, checkNumber = "check_number", vendor, memo, amount, type, isReconciled = "is_reconciled"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let CONTAINER = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if CONTAINER.contains(.date) {
+            let DATE_STRING = try CONTAINER.decode(String.self, forKey: .date)
+            if let TRANSACTION_DATE = Event.DF.date(from: DATE_STRING) {
+                date = TRANSACTION_DATE
+            }
+        }
+        
+        if CONTAINER.contains(.checkNumber) {
+            checkNumber = try CONTAINER.decode(Int.self, forKey: .checkNumber)
+        }
+        
+        vendor = try CONTAINER.decode(String.self, forKey: .vendor)
+        
+        if CONTAINER.contains(.memo) {
+            memo = try CONTAINER.decode(String.self, forKey: .memo)
+        }
+        
+        amount = try CONTAINER.decode(Double.self, forKey: .amount)
+        
+        type = try CONTAINER.decode(EventType.self, forKey: .type)
+        
+        if CONTAINER.contains(.isReconciled) {
+            isReconciled = try CONTAINER.decode(Bool.self, forKey: .isReconciled)
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(Event.DF.string(from: date), forKey: .date)
+        
+        if let checkNumber = checkNumber {
+            try container.encode(checkNumber, forKey: .checkNumber)
+        }
+        
+        try container.encode(vendor, forKey: .vendor)
+        
+        if !memo.isEmpty {
+            try container.encode(memo, forKey: .memo)
+        }
+        
+        try container.encode(amount, forKey: .amount)
+        
+        try container.encode(type, forKey: .type)
+        
+        if isReconciled {
+            try container.encode(isReconciled, forKey: .isReconciled)
+        }
+    }
+}
