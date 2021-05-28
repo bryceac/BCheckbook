a3cd80f6657cb086f24eaec3d47be4facaa435e9
Bryce Campbell <tonyhawk2100@gmail.com>
2021-05-27 22:09:04 -0700

    work on view for app.

--- iOS/RecordView.swift
+++ iOS/RecordView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct RecordView: View {
+    @ObservedObject var record: Record
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(Event.DF.string(from: record.transaction.date))
+            
+            if let checkNumber = record.transaction.checkNumber {
+                Text("\(checkNumber)")
+            }
+            
+            Text(record.transaction.isReconciled ? "Y" : "N")
+            
+            VStack {
+                Text(record.transaction.vendor)
+                Text(record.transaction.memo)
+            }
+            
+            switch record.transaction.type {
+            case .deposit:
+                if let value = Event.CURRENCY_FORMAT.string(from: record.transaction.amount as NSNumber) {
+                    Text(value)
+                }
+                
+                Text("N/A")
+            case .withdrawal:
+                Text("N/A")
+                
+                if let value = Event.CURRENCY_FORMAT.string(from: record.transaction.amount as NSNumber) {
+                    Text(value)
+                }
+            }
+            
+            if let value = Event.CURRENCY_FORMAT.string(from: record.balance as NSNumber) {
+                Text(value)
+            }
+        }
     }
 }
 
 struct RecordView_Previews: PreviewProvider {
     static var previews: some View {
-        RecordView()
+        RecordView(record: Record())
     }
 }
