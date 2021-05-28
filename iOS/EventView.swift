a9a8cbd48a2cbb7f0745320d52f1680fa5397ad4
Bryce Campbell <tonyhawk2100@gmail.com>
2021-05-27 21:59:53 -0700

    add more view files for iOS.

---
+++ iOS/EventView.swift
@@ -0,0 +1,20 @@
+//
+//  EventView.swift
+//  Checkbook (iOS)
+//
+//  Created by Bryce Campbell on 5/27/21.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventView()
+    }
+}
---
+++ iOS/RecordView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordView.swift
+//  Checkbook (iOS)
+//
+//  Created by Bryce Campbell on 5/27/21.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView()
+    }
+}
