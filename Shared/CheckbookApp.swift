6289bc4cd430656239b30f0b286260b7bb631c91
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-30 16:32:05 -0700

    allow opening files.

--- Shared/CheckbookApp.swift
+++ Shared/CheckbookApp.swift
@@ -13,11 +13,19 @@ struct CheckbookApp: App {
     
     @State var file: URL? = nil
     
+    let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(records)
         }.commands {
             #if os (macOS)
+            CommandGroup(before: CommandGroupPlacement.newItem) {
+                Button("Open") {
+                    open()
+                }
+            }
+            
             CommandGroup(after: CommandGroupPlacement.newItem) {
                 Button("Save") {
                     if let file = file {
@@ -32,8 +40,6 @@ struct CheckbookApp: App {
     }
     
     func saveAs() {
-        let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        
         let SAVE_PANEL = NSSavePanel()
         SAVE_PANEL.allowedFileTypes = ["json"]
         SAVE_PANEL.showsHiddenFiles = true
@@ -45,6 +51,22 @@ struct CheckbookApp: App {
             if case NSApplication.ModalResponse.OK = result, let filePath = SAVE_PANEL.url {
                 try? records.items.save(to: filePath)
                 
+                file = filePath
+            }
+        }
+    }
+    
+    func open() {
+        let OPEN_PANEL = NSOpenPanel()
+        OPEN_PANEL.allowedFileTypes = ["json"]
+        OPEN_PANEL.showsHiddenFiles = true
+        OPEN_PANEL.canChooseDirectories = true
+        OPEN_PANEL.showsResizeIndicator = true
+        OPEN_PANEL.allowsMultipleSelection = false
+        OPEN_PANEL.directoryURL = DOCUMENTS_DIRECTORY
+        OPEN_PANEL.begin { result in
+            if case NSApplication.ModalResponse.OK = result, let filePath = OPEN_PANEL.url, let SAVED_RECORDS = try? Record.load(from: filePath) {
+                records.items = SAVED_RECORDS
                 file = filePath
             }
         }
