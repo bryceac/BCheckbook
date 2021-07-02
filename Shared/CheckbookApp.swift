5eb4b4a6bf5d21c7251a8f6c7b1f6f0cb0f3a498
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-02 09:44:39 -0700

    fix error in code.

--- Shared/CheckbookApp.swift
+++ Shared/CheckbookApp.swift
@@ -21,7 +21,7 @@ struct CheckbookApp: App {
             let APP_NAME = CFBundleGetValueForInfoDictionaryKey(Bundle.main as! CFBundle, "CFBundleDisplayName" as CFString) as? String
             
             if let filePath = file, let displayName = APP_NAME {
-                ContentView().environmentObject(records).navigationTitle("\(filePath.absoluteString) - \(APP_NAME)")
+                ContentView().environmentObject(records).navigationTitle("\(filePath.absoluteString) - \(displayName)")
             } else {
                 ContentView().environmentObject(records)
             }
