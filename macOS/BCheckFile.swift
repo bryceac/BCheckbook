c36eb7ff74b59b5686f43de6b636345e8a0cba02
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-08 15:46:27 -0700

    type working on Document type.

--- macOS/BCheckFile.swift
+++ macOS/BCheckFile.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UniformTypeIdentifiers
+import SwiftUI
+
+struct BCheckFile: FileDocument {
+    static var readableContentTypes: [UTType] = [.json]
+    
+    var records: Records = Records()
+    
+    init(configuration: ReadConfiguration) throws {
+        let SAVED_RECORDS = Record.load(from: configuration.file.)
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        <#code#>
+    }
+
+}
