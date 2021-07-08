0a6a3e33851af6ee60cff7deda91c6c05354c226
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-08 16:10:08 -0700

    get App file back to basics.

--- macOS/CheckbookApp.swift
+++ macOS/CheckbookApp.swift
@@ -9,28 +9,11 @@ import SwiftUI
 
 @main
 struct CheckbookApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
-    @StateObject var records: Records = Records()
-    
-    @State var file: URL? = nil
-    @State var appAlert: AppAlert? = nil
-    
-    let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-    
     var body: some Scene {
         WindowGroup {
-            if let filePath = file, let displayName = Bundle.main.displayName {
-                ContentView().environmentObject(records).navigationTitle("\(filePath.path) - \(displayName)").alert(item: $appAlert) { activeAlert in
-                    activeAlert.alert
-                }
-            } else if let displayName = Bundle.main.displayName {
-                ContentView().environmentObject(records).navigationTitle("New Register - \(displayName)").alert(item: $appAlert) { activeAlert in
-                    activeAlert.alert
-                }
-            }
+            ContentView()
         }.commands {
-            CommandGroup(replacing: CommandGroupPlacement.newItem) {
+            /* CommandGroup(replacing: CommandGroupPlacement.newItem) {
                 Button("New") {
                     appAlert = AppAlert.creatingNewFile(confirmHandler: {
                         appAlert = nil
@@ -83,60 +66,7 @@ struct CheckbookApp: App {
                 Button("Save As") {
                     saveAs()
                 }.keyboardShortcut(KeyEquivalent("s"), modifiers: [.option, .command, .shift])
-            }
-        }
-    }
-    
-    func save() {
-        if let file = file {
-            try? records.sortedRecords.save(to: file)
-        } else {
-            saveAs()
-        }
-    }
-    
-    func saveAs() {
-        let SAVE_PANEL = NSSavePanel()
-        SAVE_PANEL.allowedFileTypes = ["bcheck"]
-        SAVE_PANEL.showsHiddenFiles = true
-        SAVE_PANEL.showsResizeIndicator = true
-        SAVE_PANEL.canCreateDirectories = true
-        SAVE_PANEL.directoryURL = DOCUMENTS_DIRECTORY
-        SAVE_PANEL.nameFieldStringValue = "transactions"
-        SAVE_PANEL.begin { result in
-            if case NSApplication.ModalResponse.OK = result, let filePath = SAVE_PANEL.url {
-                try? records.sortedRecords.save(to: filePath)
-                
-                file = filePath
-            }
-        }
-    }
-    
-    func open() {
-        records.clear()
-        let OPEN_PANEL = NSOpenPanel()
-        OPEN_PANEL.allowedFileTypes = ["bcheck"]
-        OPEN_PANEL.showsHiddenFiles = true
-        OPEN_PANEL.canChooseDirectories = true
-        OPEN_PANEL.showsResizeIndicator = true
-        OPEN_PANEL.allowsMultipleSelection = false
-        OPEN_PANEL.directoryURL = DOCUMENTS_DIRECTORY
-        OPEN_PANEL.begin { result in
-            if case NSApplication.ModalResponse.OK = result, let filePath = OPEN_PANEL.url, let SAVED_RECORDS = try? Record.load(from: filePath) {
-                
-                for record in SAVED_RECORDS {
-                    records.add(record)
-                }
-                
-                file = filePath
-            }
-        }
-    }
-    
-    func new() {
-        DispatchQueue.main.async {
-            records.clear()
-            file = nil
+            } */
         }
     }
 }
