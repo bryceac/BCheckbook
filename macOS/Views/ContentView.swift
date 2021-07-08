f6859bfb060af4aa2806ad78a1a313f1cfe9d560
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-08 16:19:56 -0700

    make empty initializer.

--- macOS/Views/ContentView.swift
+++ macOS/Views/ContentView.swift
@@ -8,20 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var records: Records
+    @Binding var bcheckFile: BCheckFile
+    
     var body: some View {
         List {
-            ForEach(records.sortedRecords.indices, id: \.self) { index in
-                RecordView(record: records.sortedRecords[index]).contextMenu(ContextMenu(menuItems: {
+            ForEach(bcheckFile.records.sortedRecords.indices, id: \.self) { index in
+                RecordView(record: bcheckFile.records.sortedRecords[index]).contextMenu(ContextMenu(menuItems: {
                     Button("Delete") {
-                        records.remove(at: index)
+                        bcheckFile.records.remove(at: index)
                     }
                 }))
             }
         }.toolbar(content: {
             ToolbarItem(placement: ToolbarItemPlacement.primaryAction) {
                 Button("+") {
-                    records.add(Record())
+                    bcheckFile.records.add(Record())
                 }
             }
         })
@@ -30,6 +31,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environmentObject(Records())
+        ContentView(bcheckFile: .constant(BCheckFile()))
     }
 }
