fbfdb9f009ac21620c6c7cf70fd694eb2a69c9ed
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-10 11:58:56 -0700

    add extension to remove record by element.

---
+++ macOS/Extensions/Records+removeRecord.swift
@@ -0,0 +1,14 @@
+//
+//  Records+removeRecord.swift
+//  BCheckbook (macOS)
+//
+//  Created by Bryce Campbell on 7/10/21.
+//
+
+import Foundation
+
+extension Records {
+    func remove(_ record: inout Record) {
+        guard let RECORD_INDEX = self.items.firstIndex(of: Record)
+    }
+}
