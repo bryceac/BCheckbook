6fb9a53ede2b9e945a432f6a7446623d9f4416b1
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-17 11:29:37 -0800

    attempt to create databasee manager object.

--- iOS/CheckbookApp.swift
+++ iOS/CheckbookApp.swift
@@ -10,10 +10,21 @@ import SwiftUI
 @main
 struct CheckbookApp: App {
     @StateObject var records: Records = Records()
+    @StateObject var databaseManager: DBManager
+    
+    let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
     
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(records)
+        }
+    }
+    
+    init() {
+        do {
+            _databaseManager = try StateObject(wrappedValue: DBManager(withDB: DOCUMENTS_DIRECTORY.appendingPathComponent("register").appendingPathExtension("db")))
+        } catch (let error) {
+            print("Cannot open database")
         }
     }
 }
