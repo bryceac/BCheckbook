aa55bd699d772015333d69aad9724a4ded216cd6
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-18 16:35:03 -0800

    create protocol to refresh view.

---
+++ iOS/RecordDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  RecordDelegate.swift
+//  BCheckbook (iOS)
+//
+//  Created by Bryce Campbell on 12/18/21.
+//
+
+import Foundation
+protocol RecordDelegate {
+    func refreshListView()
+}
