1f22359f751221055d9f003d540eae18c09f41ab
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-18 18:28:31 -0800

    use super observing class again.

--- iOS/CheckbookApp.swift
+++ iOS/CheckbookApp.swift
@@ -10,9 +10,11 @@ import SwiftUI
 @main
 struct CheckbookApp: App {
     
+    @StateObject var records: Records = Records()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(records)
         }
     }
 }
