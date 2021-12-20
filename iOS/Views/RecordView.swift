2e158307f6f5ffc77854c2faf25e84d4b1b77930
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-20 10:04:27 -0800

    modify view to include category.

--- iOS/Views/RecordView.swift
+++ iOS/Views/RecordView.swift
@@ -11,7 +11,48 @@ struct RecordView: View {
     @ObservedObject var record: Record
     
     var body: some View {
-        HStack {
+        VStack {
+            HStack {
+                Text(Event.DF.string(from: record.event.date))
+                
+                if let checkNumber = record.event.checkNumber {
+                    Text("\(checkNumber)")
+                }
+                
+                Text(record.event.isReconciled ? "Y" : "N")
+                
+                VStack {
+                    Text(record.event.vendor)
+                    Text(record.event.memo)
+                }
+                
+                switch record.event.type {
+                case .deposit:
+                    if let value = Event.CURRENCY_FORMAT.string(from: record.event.amount as NSNumber) {
+                        Text(value)
+                    }
+                    
+                    Text("N/A")
+                case .withdrawal:
+                    Text("N/A")
+                    
+                    if let value = Event.CURRENCY_FORMAT.string(from: record.event.amount as NSNumber) {
+                        Text(value)
+                    }
+                }
+                
+                if let VALUE = Event.CURRENCY_FORMAT.string(from: NSNumber(value: record.balance)) {
+                    Text(VALUE)
+                }
+            }
+            
+            HStack {
+                Text(record.event.category)
+            }
+        }.onAppear {
+            record.loadbalance()
+        }
+        /* HStack {
             Text(Event.DF.string(from: record.event.date))
             
             if let checkNumber = record.event.checkNumber {
@@ -45,7 +86,7 @@ struct RecordView: View {
             }
         }.onAppear {
             record.loadbalance()
-        }
+        } */
     }
 }
 
