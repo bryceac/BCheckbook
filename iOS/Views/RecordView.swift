6981f09a76b698ba36416801996fe408838ad851
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-20 13:45:55 -0800

    remove commented out block.

--- iOS/Views/RecordView.swift
+++ iOS/Views/RecordView.swift
@@ -50,43 +50,9 @@ struct RecordView: View {
                 Text(record.event.category ?? "Uncategorized")
             }.padding()
         }.onAppear {
+            // make sure up to date balance is displayed
             record.loadbalance()
         }
-        /* HStack {
-            Text(Event.DF.string(from: record.event.date))
-            
-            if let checkNumber = record.event.checkNumber {
-                Text("\(checkNumber)")
-            }
-            
-            Text(record.event.isReconciled ? "Y" : "N")
-            
-            VStack {
-                Text(record.event.vendor)
-                Text(record.event.memo)
-            }
-            
-            switch record.event.type {
-            case .deposit:
-                if let value = Event.CURRENCY_FORMAT.string(from: record.event.amount as NSNumber) {
-                    Text(value)
-                }
-                
-                Text("N/A")
-            case .withdrawal:
-                Text("N/A")
-                
-                if let value = Event.CURRENCY_FORMAT.string(from: record.event.amount as NSNumber) {
-                    Text(value)
-                }
-            }
-            
-            if let VALUE = Event.CURRENCY_FORMAT.string(from: NSNumber(value: record.balance)) {
-                Text(VALUE)
-            }
-        }.onAppear {
-            record.loadbalance()
-        } */
     }
 }
 
