70d14b1ffd9c098d26360c22a1b808c2fa9e714d
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-21 10:55:59 -0800

    put database in application support on mac.

--- Shared/DB.swift
+++ Shared/DB.swift
@@ -18,6 +18,8 @@ class DB {
     static let shared = DB()
     
     private init() {
+        
+        #if os(iOS)
         let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         
         url = DOCUMENTS_DIRECTORY.appendingPathComponent("register").appendingPathExtension("db")
@@ -25,6 +27,15 @@ class DB {
         if !FileManager.default.fileExists(atPath: url.absoluteString), let BUNDLE_PATH = Bundle.main.url(forResource: url.deletingPathExtension().lastPathComponent, withExtension: url.pathExtension) {
             try? FileManager.default.copyItem(at: BUNDLE_PATH, to: url)
         }
+        #else
+        let APPLICATION_SUPPORT_DIRECTORY = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        
+        url = APPLICATION_SUPPORT_DIRECTORY.appendingPathComponent("register").appendingPathExtension("db")
+        
+        if !FileManager.default.fileExists(atPath: url.absoluteString), let BUNDLE_PATH = Bundle.main.url(forResource: url.deletingPathExtension().lastPathComponent, withExtension: url.pathExtension) {
+            try? FileManager.default.copyItem(at: BUNDLE_PATH, to: url)
+        }
+        #endif
         
         manager = try? DBManager(withDB: url)
     }
