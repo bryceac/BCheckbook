c60e122568da3cf4408a9bc4a38039baf29858cb
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-28 09:13:57 -0800

    set minimum window size to 800 x 600 on mac.

--- macOS/CheckbookApp.swift
+++ macOS/CheckbookApp.swift
@@ -13,7 +13,7 @@ struct CheckbookApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().frame(minWidth: 800, minHeight: 600)
         }
     }
 }
