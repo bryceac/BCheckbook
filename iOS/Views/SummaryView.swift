ce589f13afc04cf579955d12da49b08e9507edcc
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-08 14:26:39 -0800

    add new view for usage in app.

---
+++ iOS/Views/SummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryView.swift
+//  BCheckbook (iOS)
+//
+//  Created by Bryce Campbell on 1/8/22.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
