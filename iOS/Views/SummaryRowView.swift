2a24af47889a2c42d97efc8a3eb619ef33c8676e
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-08 14:49:09 -0800

    create view that can be reused.

---
+++ iOS/Views/SummaryRowView.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryRowView.swift
+//  BCheckbook (iOS)
+//
+//  Created by Bryce Campbell on 1/8/22.
+//
+
+import SwiftUI
+
+struct SummaryRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryRowView()
+    }
+}
