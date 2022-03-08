cdf70ba6760068a78213f805475e812984aec925
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-08 09:12:43 -0800

    try to create a view dedicated fo load screen.

---
+++ Shared/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  BCheckbook
+//
+//  Created by Bryce Campbell on 3/8/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
