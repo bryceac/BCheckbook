2db047bf44b6e8b3e0da2f0643a1a2ceef920af0
Bryce Campbell <tonyhawk2100@gmail.com>
2022-03-12 12:15:49 -0800

    add Quicken support to original document type.

--- Shared/Models/BCheckFileDocument.swift
+++ Shared/Models/BCheckFileDocument.swift
@@ -8,17 +8,23 @@
 import Foundation
 import UniformTypeIdentifiers
 import SwiftUI
+import QIF
+import SQLite
 
 extension UTType {
     static var bcheckFiles: UTType {
         UTType(importedAs: "me.brycecampbell.bcheck")
+    }
+    
+    static var quickenInterchangeFormat: UTType {
+        UTType(importedAs: "me.brycecampbell.qif")
     }
 }
 
 class BCheckFileDocument: ReferenceFileDocument {
     
     
-    static var readableContentTypes: [UTType] = [.bcheckFiles]
+    static var readableContentTypes: [UTType] = [.bcheckFiles, .quickenInterchangeFormat]
     
     var records: Records = Records()
     
@@ -29,31 +35,52 @@ class BCheckFileDocument: ReferenceFileDocument {
     required convenience init(configuration: ReadConfiguration) throws {
         var records = Records()
         
-        if let jsonData = configuration.file.regularFileContents {
-            let SAVED_RECORDS = try Record.load(from: jsonData)
+        if let fileData = configuration.file.regularFileContents {
             
-            records = Records(withRecords: SAVED_RECORDS)
+            if let fileName = configuration.file.filename, fileName.hasSuffix(".bcheck") {
+                let SAVED_RECORDS = try Record.load(from: fileData)
+                
+                records = Records(withRecords: SAVED_RECORDS)
+            } else if let content = String(data: fileData, encoding: .utf8) {
+                let qif = try QIF(content)
+                
+                if let bank = qif.sections[QIFType.bank.rawValue] {
+                    let SAVED_RECORDS = bank.transactions.map { transaction in
+                        Record(transaction: Event(transaction))
+                    }
+                    
+                    records = Records(withRecords: SAVED_RECORDS)
+                }
+            }
         }
         
         self.init(records: records)
     }
     
-    /* func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        var fileWrapper = FileWrapper()
-        
-        if let JSON_DATA = records.sortedRecords.data {
-            fileWrapper = FileWrapper(regularFileWithContents: JSON_DATA)
+    func generateQIF() -> QIF {
+        let qifTransactions = records.sortedRecords.map { record in
+            QIFTransaction(record.event)
         }
         
-        return fileWrapper
-    } */
+        let section = QIFSection(type: .bank, transactions: Set(qifTransactions))
+        
+        return QIF(sections: [section.type.rawValue: section])
+    }
     
     func fileWrapper(snapshot: [Record], configuration: WriteConfiguration) throws -> FileWrapper {
         
         var fileWrapper = FileWrapper()
         
-        if let JSON_DATA = records.sortedRecords.data {
-            fileWrapper = FileWrapper(regularFileWithContents: JSON_DATA)
+        if configuration.contentType == UTType.bcheckFiles {
+            if let JSON_DATA = records.sortedRecords.data {
+                fileWrapper = FileWrapper(regularFileWithContents: JSON_DATA)
+            }
+        } else {
+            let qif = generateQIF()
+            
+            if let qifData = "\(qif)".data(using: .utf8) {
+                fileWrapper = FileWrapper(regularFileWithContents: qifData)
+            }
         }
         
         return fileWrapper
