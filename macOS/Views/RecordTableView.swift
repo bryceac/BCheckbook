ac3c0458a24dbb2ce60715f7d99b4cc895482fbd
Bryce Campbell <tonyhawk2100@gmail.com>
2022-07-19 14:46:42 -0700

    add new file for table view.

---
+++ macOS/Views/RecordTableView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordTableView.swift
+//  BCheckbook (macOS)
+//
+//  Created by Bryce Campbell on 7/19/22.
+//
+
+import SwiftUI
+
+struct RecordTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordTableView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordTableView()
+    }
+}
