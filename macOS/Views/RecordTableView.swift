18a1daf3792093ff40ff24aec4d0e63d2177aa90
Bryce Campbell <tonyhawk2100@gmail.com>
2022-07-19 14:56:22 -0700

    get more fields setup.

--- macOS/Views/RecordTableView.swift
+++ macOS/Views/RecordTableView.swift
@@ -8,10 +8,25 @@
 import SwiftUI
 
 struct RecordTableView: View {
-    @State private var records = []
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State var records = []
     @State private var order = [
         KeyPathComparator(\Record.event.date, order: .forward)
     ]
+    
+    @State private var selectedRecords = Set<Record.ID>()
+    
+    var categoryListBinding: Binding<[String]> {
+            Binding(get: {
+                guard let databaseManager = DB.shared.manager, let categories = databaseManager.categories else { return [] }
+                return categories.sorted()
+            }, set: { newValue in
+                guard let databaseManager = DB.shared.manager else { return }
+                
+                try? databaseManager.add(categories: newValue)
+            })
+        }
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
