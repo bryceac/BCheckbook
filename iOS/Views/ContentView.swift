12374f6be26b755d3f8e69ced664ede31222bbca
Bryce Campbell <tonyhawk2100@gmail.com>
2022-11-10 12:06:29 -0800

    apply fix needed for iOS 16

--- iOS/Views/ContentView.swift
+++ iOS/Views/ContentView.swift
@@ -56,7 +56,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollViewReader { proxy in
                 List {
                     ForEach(filteredRecords) { record in
